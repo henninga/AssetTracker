312ab3f92d323df77948f647129849c537f89e39
Henning Anderssen <hightowr@hotmail.com>
2011-11-09 10:22:59 +0100

    Removed deleted files

--- Src/AssetTracker/DirectoryServices/AssetSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AssetTracker.DirectoryServices
-{
-    public class AssetSettings
-    {
-        public string LdapPath { get; set; }
-        public string UsernameToken { get; set; }
-        public string FullnameToken { get; set; }
-    }
-}
